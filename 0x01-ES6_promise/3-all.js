5cccf9bbbf0a8b2f102364b54b91f56327d74c14
Awe Ayomidipupo <pupoawe@gmail.com>
2023-06-27 23:13:31 +0100

    fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([createUser(), uploadPhoto()])
-    .then(([user, photo]) => console.log(photo.body, user.firstName, user.lastName))
+    .then(([user, photo]) => console.log(`${photo.body} ${user.firstName} ${user.lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
