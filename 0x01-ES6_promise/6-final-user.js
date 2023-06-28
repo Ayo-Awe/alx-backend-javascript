a0f02a93b0ed72202f507520af0ddcec787c0684
Awe Ayomidipupo <pupoawe@gmail.com>
2023-06-28 01:57:57 +0100

    fix: resolve bug

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,18 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  const data = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((values) => values);
+
+  return data.map((d) => ({
+    status: d.status,
+    value: d.reason ? d.reason.toString() : d.value,
+  }));
 }
