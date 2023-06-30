cc6c0587d20a73284b452b894823852b3b154643
Awe Ayomidipupo <pupoawe@gmail.com>
2023-06-30 18:41:00 +0100

    feat: solve task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,27 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== "string") {
+      throw new TypeError("Name must be a string");
+    }
+
+    if (typeof length !== "number") {
+      throw new TypeError("Length must be a number");
+    }
+
+    if (!isArrayOfStrings(students)) {
+      throw new TypeError("Students must be an array of strings");
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+}
+
+function isArrayOfStrings(value) {
+  if (!Array.isArray(value)) return false;
+
+  if (!value.every((student) => typeof student === "string")) return false;
+
+  return true;
+}
