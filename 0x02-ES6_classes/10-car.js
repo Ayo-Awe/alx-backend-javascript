eb42fc2d7334ff86cdc45f8298529c646dd6cf17
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-01 14:48:39 +0100

    feat: solve task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,40 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._color = color;
+    this._motor = motor;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+}
