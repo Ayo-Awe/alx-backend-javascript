47a52ec343648801733268d7417a914541b9c08d
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-01 12:43:02 +0100

    feat: solve task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,30 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== "number") {
+      throw new TypeError("sqft must be a number");
+    }
+
+    if (
+      this.constructor !== Building &&
+      "evacuationWarningMessage" in this === false
+    ) {
+      throw new Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("sqft must be a number");
+    }
+
+    this._sqft = value;
+  }
+}
