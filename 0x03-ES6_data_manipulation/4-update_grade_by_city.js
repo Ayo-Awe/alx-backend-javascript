36121213fc9c21009cd51616603f690826fb0d9e
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-04 12:10:09 +0100

    feat: solve task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  const studentsInCity = studentList.filter(
+    (student) => student.location === city,
+  );
+
+  return studentsInCity.map((student) => {
+    const grade = newGrades.find((grade) => grade.studentId === student.id);
+    return { ...student, grade: grade || 'N/A' };
+  });
+}
