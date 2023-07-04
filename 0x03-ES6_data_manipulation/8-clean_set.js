0f96601f3574bc5eb6459a9f53c49de1dcfe6d63
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-04 12:56:17 +0100

    fix: ignore values that are equal to startString

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 export default function cleanSet(set, startString) {
   return Array.from(set)
-    .filter((string) => string.startsWith(startString) && startString !== '')
+    .filter(
+      (string) => string.startsWith(startString)
+        && startString !== ''
+        && string !== startString,
+    )
     .map((string) => string.substring(startString.length))
     .join('-');
 }
