b0389666a6887e1962adca7a64822a5f668d8b7d
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-04 12:31:40 +0100

    refactor: use dataview to implement typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) throw new Error('Position outside range');
 
-  const typedArray = new Int8Array(length);
-  typedArray[position] = value;
+  const typedArray = new DataView(new ArrayBuffer(length));
+  typedArray.setInt8(position, value);
 
-  return new DataView(typedArray.buffer);
+  return typedArray;
 }
