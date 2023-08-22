71bb2dbe9ae2becc9adf7acc1a4b45b00e2d34b4
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-22 12:29:45 +0100

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,19 @@
-console.log("Welcome to Holberton School, what is your name?");
+function printName() {
+  console.log("Welcome to Holberton School, what is your name?");
 
-process.stdin.setEncoding("utf8");
-process.stdin.on("readable", () => {
-  let chunk = process.stdin.read();
-  if (chunk !== null) {
-    console.log(`Your name is: ${chunk.trim()}`);
-  }
-});
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("readable", () => {
+    let chunk = process.stdin.read();
+    if (chunk !== null) {
+      console.log(`Your name is: ${chunk.trim()}`);
+    }
+  });
 
-process.stdin.on("end", () => {
-  console.log("This important software is now closing");
-});
+  process.stdin.on("end", () => {
+    console.log("This important software is now closing");
+  });
+}
+
+module.exports = printName;
+
+printName();
