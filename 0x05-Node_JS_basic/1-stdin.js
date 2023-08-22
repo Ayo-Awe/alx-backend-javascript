7f312eedcad6af530fb991d1298113a7adf7a17e
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-22 12:24:00 +0100

    feat(0x05): solve task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Holberton School, what is your name?");
+
+process.stdin.setEncoding("utf8");
+process.stdin.on("readable", () => {
+  let chunk = process.stdin.read();
+  if (chunk !== null) {
+    console.log(`Your name is: ${chunk.trim()}`);
+  }
+});
+
+process.stdin.on("end", () => {
+  console.log("This important software is now closing");
+});
