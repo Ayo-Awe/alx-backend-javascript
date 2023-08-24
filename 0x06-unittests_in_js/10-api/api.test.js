488c5967fc10411b5c3865282cb81abd29356e01
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-25 00:07:37 +0100

    add tests for all endpoints

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -2,6 +2,42 @@ const { expect } = require("chai");
 const request = require("request");
 
 describe("Basic integration tests", () => {
+  describe("/", () => {
+    it("should return status 200", (done) => {
+      request.get("http://localhost:7865", (err, res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+    it("should return correct response body", (done) => {
+      request.get("http://localhost:7865", (err, res) => {
+        expect(res.body).to.equal("Welcome to the payment system");
+        done();
+      });
+    });
+  });
+
+  describe("/cart/:id", () => {
+    it("should return status 200 when id is a number", (done) => {
+      request.get("http://localhost:7865/cart/3", (err, res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+    it("should return status 404 when id is not a number", (done) => {
+      request.get("http://localhost:7865/cart/3boexes", (err, res) => {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+    });
+    it("should return correct response body", (done) => {
+      request.get("http://localhost:7865/cart/6", (err, res) => {
+        expect(res.body).to.equal("Payment methods for cart 6");
+        done();
+      });
+    });
+  });
+
   describe("/login", () => {
     it("should return status 200", (done) => {
       request.post(
