3e0cfe4774e0b0c29ef935edfda0ab10a75b5255
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-24 21:50:09 +0100

    fix

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,59 +5,59 @@ describe("calculateNumber", function () {
   describe("SUM", () => {
     it("should return correct sum when but numbers are integers", function () {
       const sum = calculateNumber("SUM", 9, 9);
-      expect(sum).eq(18);
+      expect(sum).to.equal(18);
     });
     it("should return correct sum when one number should be rounded up", function () {
       const sum = calculateNumber("SUM", 2.62, 9);
-      expect(sum).eq(12);
+      expect(sum).to.equal(12);
     });
     it("should return correct sum when one number should be rounded down", function () {
       const sum = calculateNumber("SUM", 2.32, 9);
-      expect(sum).eq(11);
+      expect(sum).to.equal(11);
     });
     it("should return correct sum when numbers are negative", function () {
       const sum = calculateNumber("SUM", -5.8, -2.2);
-      expect(sum).eq(-8);
+      expect(sum).to.equal(-8);
     });
   });
   describe("SUBTRACT", () => {
     it("should return correct value when but numbers are integers", function () {
       const sum = calculateNumber("SUBTRACT", 9, 9);
-      expect(sum).eq(0);
+      expect(sum).to.equal(0);
     });
     it("should return correct sum when one number should be rounded up", function () {
       const sum = calculateNumber("SUBTRACT", 2.62, 9);
-      expect(sum).eq(-6);
+      expect(sum).to.equal(-6);
     });
     it("should return correct sum when one number should be rounded down", function () {
       const sum = calculateNumber("SUBTRACT", 0.002, -4);
-      expect(sum).eq(4);
+      expect(sum).to.equal(4);
     });
     it("should return correct sum when numbers are negative", function () {
       const sum = calculateNumber("SUBTRACT", -5, -2);
-      expect(sum).eq(-3);
+      expect(sum).to.equal(-3);
     });
   });
   describe("DIVIDE", () => {
     it("should return correct value when but numbers are integers", function () {
       const sum = calculateNumber("DIVIDE", 9, 9);
-      expect(sum).eq(1);
+      expect(sum).to.equal(1);
     });
     it("should return correct sum when one number should be rounded up", function () {
       const sum = calculateNumber("DIVIDE", 2.62, 9);
-      expect(sum).eq(1 / 3);
+      expect(sum).to.equal(1 / 3);
     });
     it("should return correct sum when one number should be rounded down", function () {
       const sum = calculateNumber("DIVIDE", 0.002, -4);
-      expect(sum).eq(0);
+      expect(sum).to.equal(0);
     });
     it("should return correct sum when numbers are negative", function () {
       const sum = calculateNumber("DIVIDE", -5.6, -2);
-      expect(sum).eq(3);
+      expect(sum).to.equal(3);
     });
     it("should return error sum when round of b is 0", function () {
       const sum = calculateNumber("DIVIDE", -5.6, -0.003);
-      expect(sum).eq("Error");
+      expect(sum).to.equal("Error");
     });
   });
 });
