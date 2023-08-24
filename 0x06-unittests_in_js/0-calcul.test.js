774f857f4eba96a3082808fb1c2c41b1590b9a0b
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-25 00:38:09 +0100

    fix: add extra test to task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", function () {
-  it("should return correct sum when but numbers are integers", function () {
+  it("should return correct sum when both numbers are integers", function () {
     const sum = calculateNumber(9, 9);
     assert.equal(sum, 18);
   });
@@ -17,5 +17,9 @@ describe("calculateNumber", function () {
   it("should return correct sum when numbers are negative", function () {
     const sum = calculateNumber(-5.8, -2.2);
     assert.equal(sum, -8);
+  });
+  it("should return correct sum when one number is 0", function () {
+    const sum = calculateNumber(4.97, 0);
+    assert.equal(sum, 5);
   });
 });
